6d64f79041c9098e38084d73036152600ac54450
onibakuchi <etherdao0x00@gmail.com>
2023-09-28 10:43:48 +0900

    Fix

--- test/Approximation.t.sol
+++ test/Approximation.t.sol
@@ -8,31 +8,27 @@ import {PoolMath, PoolState, PoolPreCompute} from "src/libs/PoolMath.sol";
 import {ApproxParams} from "src/interfaces/ApproxParams.sol";
 import {LibApproximation} from "src/libs/LibApproximation.sol";
 
-// contract ApproxHarness {
-//     function error_function(
-//         uint256 baseLptSwap,
-//         PoolState memory state,
-//         PoolPreCompute memory comp,
-//         uint256 baseLptDeposit
-//     ) external view returns (int256) {
-//         return LibApproximation.error_function(baseLptSwap, state, comp, baseLptDeposit);
-//     }
-// }
-
 contract ApproxSwapPtToAddLiquidityUnitTest is Test {
     using SignedMath for int256;
 
-    PoolState state = PoolState({
-        totalBaseLptTimesN: 3300 * 1e18,
-        totalUnderlying18: 3000 * 1e18,
-        maturity: block.timestamp + 100 days,
-        scalarRoot: 1.2 * 1e18,
-        lnFeeRateRoot: 0.001 * 1e18,
-        protocolFeePercent: 10,
-        lastLnImpliedRate: 0.1 * 1e18
-    });
+    PoolState state;
+    PoolPreCompute comp;
 
-    PoolPreCompute comp = PoolMath.computeAmmParameters(state);
+    function setUp() public {
+        vm.warp(18228389); // block.timestamp is not relevant; choose any arbitrary value
+
+        state = PoolState({
+            totalBaseLptTimesN: 3300 * 1e18,
+            totalUnderlying18: 3000 * 1e18,
+            maturity: block.timestamp + 100 days,
+            scalarRoot: 1.2 * 1e18,
+            lnFeeRateRoot: 0.001 * 1e18,
+            protocolFeePercent: 10,
+            lastLnImpliedRate: 0.1 * 1e18
+        });
+
+        comp = PoolMath.computeAmmParameters(state);
+    }
 
     function test_error_function(uint256 baseLptSwap) public virtual {
         uint256 baseLptDeposit = 100 * 1e18;
